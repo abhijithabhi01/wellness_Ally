eacc07b3db1b5eca9a7200e2de2db2de36dd0247
subintm <subintmtm70@gmail.com>
2024-02-28 16:49:07 +0530

    bg

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React from 'react';
+import bg from '../assets/bgg.jpeg';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+
 
 function Login() {
+  const containerStyle = {
+    backgroundImage: `url(${bg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '80vh',
+    width: '30%',
+    marginTop: ' 6%', 
+    marginLeft:'35%',
+    // opacity:'35%'
+    
+  };
+
   return (
     <>
-    <div  style= {{backgroundColor:'#47476b',height:'100vh', width:'100%'}}>
-     <div className='d-flex justify-content-center align-items-center'style={{backgroundColor:'red',height:'60vh',width:'40%'}}></div>
-    </div>
+      <div className='d-flex justify-content-center align-items-center bg-dark' id="m_div" style={containerStyle}>
+      <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 2, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+     
+      <TextField id="standard-basic" label="Email" variant="standard" style={{color:'white'}}/>
+      <TextField id="standard-basic" label= "password" variant="standard" style={{color:'white'}}/>
 
+    </Box>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
