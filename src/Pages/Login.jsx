50a5f5749de13cc28e4f9ca5342a29e475b84f7a
Sanjay G <sanjaysanju7560@gmail.com>
2024-02-28 16:22:32 +0530

    bg_change

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Login() {
   return (
-<div style={{backgroundColor:'#47476b', height:'100vh',width:'100%'}}>
-     <div ></div>
-</div>
+    <>
+    <div  style= {{backgroundColor:'#47476b',height:'100vh', width:'100%'}}>
+     <div className='d-flex justify-content-center align-items-center'style={{backgroundColor:'red',height:'60vh',width:'40%'}}></div>
+    </div>
+
+    </>
   )
 }
 
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>demo</div>
+    <div  style= {{height:'100vh', width:'100%'}}>
+    <div style={{backgroundColor:'White',height:'60vh',width:'40%',marginLeft:'10%',marginTop:'30px'}}> </div>
+   </div>
   )
 }
 
