999010d5f07055dc3ef1cf9977aa1130d2fbbd12
subintm <subintmtm70@gmail.com>
2024-02-28 15:44:26 +0530

    clrchanged

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,7 +4,7 @@ function Login() {
   return (
     <>
     <div  style= {{backgroundColor:'#47476b',height:'100vh', width:'100%'}}>
-     <div className='d-flex justify-content-center align-items-center'style={{backgroundColor:'white',height:'60vh',width:'40%'}}></div>
+     <div className='d-flex justify-content-center align-items-center'style={{backgroundColor:'red',height:'60vh',width:'40%'}}></div>
     </div>
 
     </>
