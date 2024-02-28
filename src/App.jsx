ba574a38a8b7b8cdda847c62ad802462d7880c2b
subintm <subintmtm70@gmail.com>
2024-02-28 21:16:00 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './Pages/Login';
+import Login from '../src/Pages/LginForm/Login';
 import { Route, Routes } from 'react-router-dom';
 import Register from './Pages/Register';
 function App() {
